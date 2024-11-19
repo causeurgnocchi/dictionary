55ebfa6f9df5c59ba3798aee7a9e4c37492dde83
causeUrGnocchi <gkussunoki@gmail.com>
2024-11-19 09:42:00 -0300

    Checkpoint

---
+++ svelte-app/src/Header.svelte
@@ -0,0 +1,20 @@
+<script>
+    let pageTitle = 'Dictionary';
+</script>
+
+<style>
+    .page-header {
+        display: flex;
+    }
+
+    .page-title {
+        font-weight: normal;
+        font-size: 24px;
+        margin-left: 20px;
+        align-self: center;
+    }
+</style>
+
+<header class="page-header">
+    <h1 class="page-title">{pageTitle}</h1>
+</header>
---
+++ svelte-app/src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<script>
+    import Header from "../Header.svelte";
+</script>
+
+<style>
+    *, *::before, *::after {
+        box-sizing: border-box;
+        margin: 0;
+        padding: 0;
+    }
+
+    .container {
+        height: 100vh;
+        display: grid;
+        grid-template-rows: 50px 44px 1fr;
+    }
+</style>
+
+<div class="container">
+    <Header />
+</div>
