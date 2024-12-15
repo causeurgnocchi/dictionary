b585c8a0acd761cb6704d11c8b6aca9f8af5509d
causeUrGnocchi <gkussunoki@gmail.com>
2024-12-15 17:04:42 -0300

    Stick to using svelte

--- rest-api/handlers/VocabulariesHandler.go
+++ rest-api/handlers/VocabulariesHandler.go
@@ -5,10 +5,10 @@ import (
 	"net/http"
 )
 
-type VocabulariesApiHandler struct {
+type VocabulariesHandler struct {
 }
 
-func (h VocabulariesApiHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+func (h VocabulariesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	w.Header().Set("Access-Control-Allow-Origin", "http://127.0.0.1:5173")
 	json.NewEncoder(w).Encode(scrapeJisho(r.PathValue("search")))
--- rest-api/main.go
+++ rest-api/main.go
@@ -6,10 +6,6 @@ import (
 )
 
 func main() {
-	assets := http.FileServer(http.Dir("assets"))
-	http.Handle("/", &handlers.BaseHandler{})
-	http.Handle("/assets/", http.StripPrefix("/assets/", assets))
-	http.Handle("/results", http.StripPrefix("/results", &handlers.ResultsHandler{}))
-	http.Handle("/api/vocabularies/{search}", http.StripPrefix("/api/vocabularies/", &handlers.VocabulariesApiHandler{}))
+	http.Handle("/api/vocabularies/{search}", http.StripPrefix("/api/vocabularies/", &handlers.VocabulariesHandler{}))
 	http.ListenAndServe(":8080", nil)
 }
